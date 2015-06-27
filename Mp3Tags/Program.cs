646bdaee38217290e52b47e303173e4712d31d1c
John Zhang <15877714@qq.com>
2015-06-27 22:10:04 +0800

    Mp3

---
+++ Mp3Tags/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using WMPLib;
+namespace Mp3Tags
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WindowsMediaPlayer wmp = new WindowsMediaPlayer();
+            string path = args[0];
+            DirectoryInfo home = Directory.CreateDirectory(path);
+            path = home.FullName;
+        
+            FileInfo[] files= home.GetFiles();
+            foreach (FileInfo f in files)
+            {
+
+               IWMPMedia media= wmp.newMedia(f.FullName);
+                string name = media.getItemInfo("Title");
+                Console.WriteLine(name);
+                f.MoveTo(path+"\\"+name+f.Extension);
+                                 
+            }           
+        }
+    }
+}
